97b714ed4363bbd2c49bc64aff5bdcb4b10f53c7
Dipu Haider <dipuhaider@gmail.com>
2025-06-21 16:41:25 +0600

    Module 2.5 PD 01

---
+++ Module 2.5 PD01/F_Reversing.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int N;
+    cin >> N;
+    vector<int> vec(N);
+    for(int i = 0; i < N; i++) {
+        cin >> vec[i];
+    }
+
+    sort(vec.begin(), vec.end());
+    reverse(vec.begin(), vec.end());
+
+    for(int i = 0; i < N; i++) {
+        cout << vec[i] << " ";
+    }
+    
+    return 0;
+}
+
+//input 
+// 4
+// 5 1 3 2
+//output
+// 5 3 2 1
